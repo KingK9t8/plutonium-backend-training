80c08d09ab6d3872c53e5407f722d5592f861745
KingK9t8 <ksagarm789@gmail.com>
2022-08-08 21:52:31 +0530

    Get-API

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,38 +1,47 @@
-const express = require('express');
-const abc = require('../introduction/intro')
+const express = require("express");
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    logger.welcome()
+const movie = require("../Movies/moviesArray");
+const moviesObject = require("../Movies/moviesObject");
+// *Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
+router.get('/movies',function(req,res){
+res.send(movie.movies)
+})
 
-    res.send('My second ever api!')
+// // *Create an API GET /movies/:indexNumber
+// router.get("/movies/:indexNumber", function (req, res) {
+// 	const i = req.params;
+// 	const requiredMovie = i.indexNumber;
+// 	res.send(movie.movies[Number(requiredMovie)]);
+// });
+
+// * Handle a scenario in problem 2 where if the index is greater than the valid maximum value a message is returned that tells the user to use a valid index in an error message.
+router.get("/movies/:indexNumber", function (req, res) {
+	const i = req.params;
+	const requiredMovie = i.indexNumber;
+	if (Number(requiredMovie) < movie.movies.length) {
+		res.send(movie.movies[Number(requiredMovie)]);
+	} else {
+		res.send("Couldn't find any movie!!!");
+	}
 });
 
-router.get('/students', function (req, res){
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
-})
+// // *Write another api called GET /films. Instead of an array of strings define an array of movie objects this time. Each movie object should have values - id, name.
+// router.get("/films", function (req, res) {
+// 	res.send(moviesObject.movies_object);
+// });
 
-router.get('/student-details/:name', function(req, res){
-    /*
-    params is an attribute inside request that contains 
-    dynamic values.
-    This value comes from the request url in the form of an 
-    object where key is the variable defined in code 
-    and value is what is sent in the request
-    */
-
-    let requestParams = req.params
-
-    // JSON strigify function helps to print an entire object
-    // We can use any ways to print an object in Javascript, JSON stringify is one of them
-    console.log("This is the request "+ JSON.stringify(requestParams))
-    let studentName = requestParams.name
-    console.log('Name of the student is ', studentName)
-    
-    res.send('Dummy response')
-})
+// // *Write api GET /films/:filmId where filmId is the value received in request path params. Use this value to return a movie object with this id. In case there is no such movie present in the array, return a suitable message in the response body. ‘No movie exists with this id’
+// router.get("/films/:filmId", function (req, res) {
+// 	const i = req.params;
+// 	const requiredMovie = i.filmId - 1;
+// 	if (requiredMovie < moviesObject.movies_object.length) {
+// 		res.send(moviesObject.movies_object[Number(requiredMovie)]);
+// 	} else if (requiredMovie < 0) {
+// 		res.send("No movie exists with this id");
+// 	} else {
+// 		res.send("No movie exists with this id");
+// 	}
+// });
 
 module.exports = router;

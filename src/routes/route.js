4a0b0e2772e82cf2a85c9736f68a40ce2bffc518
KingK9t8 <ksagarm789@gmail.com>
2022-08-08 16:07:50 +0530

    New

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,35 +1,26 @@
-const express = require('express');
-const _ = require('underscore')
+const express = require("express");
+const lodash = require("lodash");
 
-const abc = require('../introduction/intro')
-const loggerModule = require('../logger/logger.js')
-const formatterModule = require('../validator/formatter') 
-const helperModule = require('../util/helper')
-const router = express.Router();
+const chunk = require("../chunk/chunk");
+const tail = require("../tail/tail");
+const union = require("../union/union");
+const fromPairs = require("../fromPairs/fromPairs");
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    loggerModule.printInfo()
-    formatterModule.trimMyString()
-    formatterModule.getUpperCaseString()
-    formatterModule.changetoLowerCase()
-    helperModule.getTodaysDate()
-    helperModule.getCurrentMonth()
-    helperModule.printBatchDetails()
-    let weekdend = ['Saturday','Sunday','Monday']
-    let result = _.first(weekdend, 2)
-    console.log('Unserscore example resultr is ',result)
-    res.send('My second ever api!')
+const router = express.Router() ;
+
+router.get("/test-me", function (req, res) {
+	console.log(lodash.chunk(chunk.month, 3));
+	console.log(lodash.tail(tail.oddNumbers));
+	console.log(lodash.union(union.num));
+	console.log(lodash.fromPairs(fromPairs.moviePairs));
+
+	res.send("My second ever api!");
 });
 
+router.get("/test-you", function (req, res) {
+	res.send("This is the second routes implementation");
+});
 
-router.get('/test-you', function(req, res){
-    res.send('This is the second routes implementation')
-})
-
-router.get('/give-me-students-data',function(req, res){
-
-})
+router.get("/give-me-students-data", function (req, res) {});
 module.exports = router;
 // adding this comment for no reason

b187ee6d548e6e1f42f9fb77c4a64df05b5c04d7
KingK9t8 <ksagarm789@gmail.com>
2022-08-09 11:28:21 +0530

    Updated logic

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,11 @@ const router = express.Router();
 
 const movie = require("../Movies/moviesArray");
 const moviesObject = require("../Movies/moviesObject");
-// *Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
-router.get('/movies',function(req,res){
-res.send(movie.movies)
-})
+
+// // *Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
+// router.get('/movies',function(req,res){
+// res.send(movie.movies)
+// })
 
 // // *Create an API GET /movies/:indexNumber
 // router.get("/movies/:indexNumber", function (req, res) {
@@ -15,16 +16,19 @@ res.send(movie.movies)
 // 	res.send(movie.movies[Number(requiredMovie)]);
 // });
 
-// * Handle a scenario in problem 2 where if the index is greater than the valid maximum value a message is returned that tells the user to use a valid index in an error message.
-router.get("/movies/:indexNumber", function (req, res) {
-	const i = req.params;
-	const requiredMovie = i.indexNumber;
-	if (Number(requiredMovie) < movie.movies.length) {
-		res.send(movie.movies[Number(requiredMovie)]);
-	} else {
-		res.send("Couldn't find any movie!!!");
-	}
-});
+// // * Handle a scenario in problem 2 where if the index is greater than the valid maximum value a message is returned that tells the user to use a valid index in an error message.
+// router.get("/movies/:indexNumber", function (req, res) {
+// 	const i = req.params;
+// 	const requiredMovie = i.indexNumber;
+// 	if (
+// 		Number(requiredMovie) >= movie.movies.length ||
+// 		Number(requiredMovie) < 0
+// 	) {
+// 		res.send("Couldn't find any movie!!!");
+// 	} else {
+// 		res.send(movie.movies[Number(requiredMovie)]);
+// 	}
+// });
 
 // // *Write another api called GET /films. Instead of an array of strings define an array of movie objects this time. Each movie object should have values - id, name.
 // router.get("/films", function (req, res) {
@@ -35,12 +39,10 @@ router.get("/movies/:indexNumber", function (req, res) {
 // router.get("/films/:filmId", function (req, res) {
 // 	const i = req.params;
 // 	const requiredMovie = i.filmId - 1;
-// 	if (requiredMovie < moviesObject.movies_object.length) {
-// 		res.send(moviesObject.movies_object[Number(requiredMovie)]);
-// 	} else if (requiredMovie < 0) {
+// 	if (requiredMovie >= moviesObject.movies_object.length || requiredMovie < 0) {
 // 		res.send("No movie exists with this id");
 // 	} else {
-// 		res.send("No movie exists with this id");
+// 		res.send(moviesObject.movies_object[Number(requiredMovie)]);
 // 	}
 // });
 

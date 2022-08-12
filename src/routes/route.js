a63e735769b964fb875502a4965db402513f9b3e
KingK9t8 <ksagarm789@gmail.com>
2022-08-12 14:47:40 +0530

    post-api-assignment-2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,121 +1,54 @@
 const express = require("express");
 const router = express.Router();
 
-// * Write a POST /players api that creates a new player ( i.e. that saves a player’s details and doesn’t allow saving the data of a player with a name that already exists in the data)
+//* Take input in query param as votingAge and for all the people above that age, change votingStatus as true. Also return an array consisting of only the person that can vote.
 
-let players = [
+let persons = [
 	{
-		name: "manish",
-		dob: "1/1/1995",
-		gender: "male",
-		city: "jalandhar",
-		sports: ["swimming"],
+		name: "PK",
+		age: 10,
+		votingStatus: false,
 	},
 	{
-		name: "gopal",
-		dob: "1/09/1995",
-		gender: "male",
-		city: "delhi",
-		sports: ["soccer"],
+		name: "SK",
+		age: 20,
+		votingStatus: false,
 	},
 	{
-		name: "lokesh",
-		dob: "1/1/1990",
-		gender: "male",
-		city: "mumbai",
-		sports: ["soccer"],
+		name: "AA",
+		age: 70,
+		votingStatus: false,
+	},
+	{
+		name: "SC",
+		age: 5,
+		votingStatus: false,
+	},
+	{
+		name: "HO",
+		age: 40,
+		votingStatus: false,
 	},
 ];
 
-router.post("/players", function (req, res) {
-	//LOGIC WILL COME HERE
-	let newPlayer = req.body;
-	let newPlayerName = newPlayer.name;
-	// adding a flag
-	let isNameRepeated = false;
-	for (player of players) {
-		if (player.name === newPlayerName) {
-			isNameRepeated = true;
-			break;
-		}
-	}
-	isNameRepeated
-		? res.send("Player exists with this name. Try another name.")
-		: players.push(newPlayer);
-	res.send({ data: players, status: true });
+router.post("/can-vote", function (req, res) {
+	const votingAge = req.query.votingAge;
+	const canVote = persons.map((person) => {
+		person.age >= votingAge
+			? (person.votingStatus = true)
+			: (person.votingStatus = false);
+	});
+	res.send({ data: canVote });
 });
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// router.post("/post-query-num", function (req, res) {
-// 	let requiredNum = req.query.number;
-// 	let finalArr = arr.filter((x) => x > requiredNum);
-// 	res.send(finalArr);
-// });
-
-// router.get("/students/:name", function (req, res) {
-// 	let studentName = req.params.name;
-// 	console.log(studentName);
-// 	res.send(studentName);
-// });
-
-// router.get("/random", function (req, res) {
-// 	res.send("hi there");
-// });
-
-// router.get("/test-api", function (req, res) {
-// 	res.send("hi FunctionUp");
-// });
-
-// router.get("/test-api-2", function (req, res) {
-// 	res.send("hi FunctionUp. This is another cool API");
-// });
-
-// router.get("/test-api-3", function (req, res) {
-// 	res.send(
-// 		"hi FunctionUp. This is another cool API. And NOw i am bored of creating API's "
-// 	);
-// });
-
-// router.get("/test-api-4", function (req, res) {
-// 	res.send(
-// 		"hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s "
-// 	);
-// });
-
-// router.get("/test-api-5", function (req, res) {
-// 	res.send(
-// 		"hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s "
-// 	);
-// });
-
-// router.get("/test-api-6", function (req, res) {
-// 	res.send({ a: 56, b: 45 });
-// });
-
-// router.post("/test-post", function (req, res) {
-// 	res.send([23, 45, 6]);
-// });
-
-// router.post("/test-post-2", function (req, res) {
-// 	res.send({ msg: "hi", status: true });
-// });
-
-// router.post("/test-post-3", function (req, res) {
-// 	// let id = req.body.user
-// 	// let pwd= req.body.password
-
-// 	// console.log( id , pwd)
-
-// 	console.log(req.body);
-
-// 	res.send({ msg: "hi", status: true });
-// });
-
-// router.post("/test-post-4", function (req, res) {
-// 	let arr = [12, "functionup"];
-// 	let ele = req.body.element;
-// 	arr.push(ele);
-// 	res.send({ msg: arr, status: true });
-// });
+router.post("/voters", function (req, res) {
+	const votingAge = req.query.votingAge;
+	for (person of persons) {
+		person.age >= votingAge
+			? (person.votingStatus = true)
+			: (person.votingStatus = false);
+	}
+	const voters = persons.filter((person) => person.age >= votingAge);
+	res.send({ data: voters });
+});
 
 module.exports = router;

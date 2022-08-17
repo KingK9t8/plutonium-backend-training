0922d1810abc90dd2425a6c0b97724e9139ee148
KingK9t8 <ksagarm789@gmail.com>
2022-08-17 11:49:42 +0530

    Modified a bit

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -16,42 +16,45 @@ const bookList = async function (req, res) {
 };
 
 const getParticularBooks = async function (req, res) {
-	const byYear = req.query.byYear;
-	const isAvailable = req.query.isAvailable;
-	const price = req.query.price;
-	const byBookName = req.query.byBookName;
-	const byAuthorName = req.query.byAuthorName;
-	if (typeof byYear === "string") {
-		let allBooks = await BookSchema.find({ year: byYear }).select({
-			bookName: 1,
-			_id: 0,
-		});
-		res.send({ data: allBooks });
-	}
-	if (isAvailable) {
-		let allBooks = await BookSchema.find({
-			stockAvailabe: isAvailable,
-		}).count();
-		res.send({ msg: allBooks });
-	}
-	if (typeof price === "string") {
-		let allBooks = await BookSchema.find({ "price.IndianPrice": price });
-		res.send(allBooks);
-	}
-	if (typeof byBookName === "string") {
-		let allBooks = await BookSchema.find({ bookName: byBookName });
-		res.send({
-			data: allBooks,
-		});
-	}
-	if (typeof byAuthorName === "string") {
-		let allBooks = await BookSchema.find({ authorName: byAuthorName }).select({
-			bookName: 1,
-		});
-		res.send(allBooks);
-	}
-	let byKeyword = await BookSchema.find({ bookName: /er$/i });
-	res.send({ books: byKeyword });
+	let byQuery = await BookSchema.find(req.body);
+	res.send({ data: byQuery });
+	// const byYear = req.query.byYear;
+	// const isAvailable = req.query.isAvailable;
+	// const price = req.query.price;
+	// const byBookName = req.query.byBookName;
+	// const byAuthorName = req.query.byAuthorName;
+
+	// if (typeof byYear === "string") {
+	// 	let allBooks = await BookSchema.find({ year: byYear }).select({
+	// 		bookName: 1,
+	// 		_id: 0,
+	// 	});
+	// 	res.send({ data: allBooks });
+	// }
+	// if (isAvailable) {
+	// 	let allBooks = await BookSchema.find({
+	// 		stockAvailabe: isAvailable,
+	// 	}).count();
+	// 	res.send({ msg: allBooks });
+	// }
+	// if (typeof price === "string") {
+	// 	let allBooks = await BookSchema.find({ "price.IndianPrice": price });
+	// 	res.send(allBooks);
+	// }
+	// if (typeof byBookName === "string") {
+	// 	let allBooks = await BookSchema.find({ bookName: byBookName });
+	// 	res.send({
+	// 		data: allBooks,
+	// 	});
+	// }
+	// if (typeof byAuthorName === "string") {
+	// 	let allBooks = await BookSchema.find({ authorName: byAuthorName }).select({
+	// 		bookName: 1,
+	// 	});
+	// 	res.send(allBooks);
+	// }
+	// let byKeyword = await BookSchema.find({ bookName: /er$/i });
+	// res.send({ books: byKeyword });
 };
 
 const getXINRBooks = async function (req, res) {

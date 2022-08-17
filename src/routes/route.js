00b213a021ff707189f6acfea83df6e0b8a030bf
KingK9t8 <ksagarm789@gmail.com>
2022-08-17 15:19:57 +0530

    modified

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const createBook=require("../controllers/bookController")
-const bookList=require("../controllers/bookController")
-const getXINRBooks=require("../controllers/bookController")
-const getRandomBooks=require("../controllers/bookController")
-const getParticularBooks=require("../controllers/bookController")
+const BookController=require("../controllers/bookController")
 
-router.post("/createBook",createBook.createBook)
-router.get("/bookList",bookList.bookList)
-router.post("/getXINRBooks",getXINRBooks.getXINRBooks)
-router.post("/getRandomBooks",getRandomBooks.getRandomBooks)
-router.post("/getParticularBooks",getParticularBooks.getParticularBooks)
+router.post("/createBook",BookController.createBook)
+router.get("/bookList",BookController.bookList)
+router.post("/getXINRBooks",BookController.getXINRBooks)
+router.post("/getRandomBooks",BookController.getRandomBooks)
+router.post("/getParticularBooks",BookController.getParticularBooks)
 
 module.exports = router ;

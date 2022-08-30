0a3a79a0660943f623b3b9490d2eef1be609eddb
KingK9t8 <ksagarm789@gmail.com>
2022-08-30 20:56:06 +0530

    http codes and try catch methods

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,61 +2,80 @@ const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (req, res) {
-	let data = req.body;
-	if (!data.mobile) return res.send("Please provide a mobile number");
-	const newUserData = await userModel.create(data);
-	res.send({ newUserData: newUserData });
+	try {
+		let data = req.body;
+		if (!data.mobile) return res.status(400).send("Please provide a mobile number");
+		const newUserData = await userModel.create(data);
+		res.status(201).send({ newUserData: newUserData });
+	} catch (error) {
+		res.status(500).send({ msg: "Internal Server Error", error: error });
+	}
 };
 
 const userLogin = async function (req, res) {
-	const emailId = req.body.emailId;
-	const password = req.body.password;
-	const user = await userModel.findOne({
-		emailId: emailId,
-		password: password,
-	});
-	if (!user) return res.send("No such user exists");
-	const newToken = jwt.sign(
-		{
-			userId: user._id.toString(),
-			batch: "Plutonium",
-			organisation: "FunctionUp",
-		},
-		"secret token for plutonium"
-	);
-	res.header("Token", newToken);
-	res.send({ data: newToken, status: true });
+	try {
+		const emailId = req.body.emailId;
+		const password = req.body.password;
+		const user = await userModel.findOne({
+			emailId: emailId,
+			password: password,
+		});
+		if (!user) return res.status(400).send("Email Id or Password is incorrect");
+		const newToken = jwt.sign(
+			{
+				userId: user._id.toString(),
+				batch: "Plutonium",
+				organisation: "FunctionUp",
+			},
+			"secret token for plutonium"
+		);
+		res.header("Token", newToken);
+		res.status(201).send({ data: newToken, status: true });
+	} catch (error) {
+		res.status(500).send({ msg: "Internal Server Error", error: error });
+	}
 };
 
 const getUserDetails = async function (req, res) {
-	const userId=req.params.userId
-	const userData = await userModel.findById(userId);
-	res.send({ status: true, userData: userData });
+	try {
+		const userId = req.params.userId;
+		const userData = await userModel.findById(userId);
+		res.status(202).send({ status: true, userData: userData });
+	} catch (error) {
+		res.status(500).send({ msg: "Internal Server Error", error: error });
+	}
 };
 
 const updateUserData = async function (req, res) {
-	const userUpdationData = req.body;
-	const userUpdatedData = await userModel.findByIdAndUpdate(
-		userId,
-		userUpdationData,
-		{ new: true }
-	);
-	if (!userUpdatedData) return res.send("No user data found");
-	res.send({ status: true, updatedData: userUpdatedData });
+	try {
+		const userId = req.params.userId;
+		const userUpdationData = req.body;
+		const userUpdatedData = await userModel.findByIdAndUpdate(
+			userId,
+			userUpdationData,
+			{ new: true }
+		);
+		if (!userUpdatedData) return res.status(404).send("No user data found");
+		res.send({ status: true, updatedData: userUpdatedData });
+	} catch (error) {
+		res.status(500).send({ msg: "Internal Server Error", error: error });
+	}
 };
 
 const deleteData = async function (req, res) {
-	const isDeleted = await userModel.findByIdAndUpdate(
-		userId,
-		{
-			isDeleted: true,
-		},
-		{ new: true }
-	);
-	if (!isDeleted) return res.send("No user data found");
-	res.send({ status: true, deletedData: isDeleted });
+	const userId = req.params.userId;
+	try {
+		const isDeleted = await userModel.findByIdAndUpdate(
+			userId,
+			{ isDeleted: true },
+			{ new: true }
+		);
+		if (!isDeleted) return res.status(404).send("No user data found");
+		res.status(202).send({ status: true, deletedData: isDeleted });
+	} catch (error) {
+		res.status(500).send({ msg: "Internal Server Error", error: error });
+	}
 };
-
 module.exports = {
 	createUser,
 	userLogin,
--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,28 +1,40 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
-
 const authJs = async function (req, res, next) {
-	let token = req.headers["x-Auth-token"];
-	if (!token) token = req.headers["x-auth-token"];
-	if (!token) return res.send("Please provide a valid token");
-	let decodedToken = jwt.verify(token, "secret token for plutonium");
-	if (!decodedToken) return res.send("Invalid Token");
-	req["verification"] = decodedToken;
-	next();
+	try {
+		let token = req.headers["x-Auth-token"];
+		if (!token) token = req.headers["x-auth-token"];
+		if (!token) return res.send("Please provide a valid token");
+		let decodedToken = jwt.verify(token, "secret token for plutonium");
+		if (!decodedToken) return res.send("Invalid Token");
+		req["verification"] = decodedToken;
+		next();
+	} catch (error) {
+		res.status(401).send({msg:"Authentication failure",error:error});
+	}
 };
 
 const authorisation = async function (req, res, next) {
-	const userId = req.params.userId;
-	if (userId !== req.verification.userId) return res.send("Not authorised");
-	next();
+	try {
+		const userId = req.params.userId;
+		if (userId !== req.verification.userId)
+			return res.status(403).send("Not authorised");
+		next();
+	} catch (error) {
+		res.status(500).send({ msg: "Internal Server Error", error: error });
+	}
 };
 
 const isUserPresent = async function (req, res, next) {
-    const userId=req.params.userId
-    const user=await userModel.findById(userId)
-    if (!user) return res.send("User not present")
-    next()
+	try {
+		const userId = req.params.userId;
+		const user = await userModel.findById(userId);
+		if (!user) return res.status(404).send("User Not Found");
+		next();
+	} catch (error) {
+		res.status(500).send({ msg: "Internal Server Error", error: error });
+	}
 };
 
-module.exports = { authJs, authorisation ,isUserPresent};
+module.exports = { authJs, authorisation, isUserPresent };

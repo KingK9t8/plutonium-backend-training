4df3d026dfc6f79fd0d98e8d1b4514d9e76bbbbd
KingK9t8 <ksagarm789@gmail.com>
2023-01-02 22:20:00 +0530

    Day 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,89 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//function component
+// const HeadingComponent = (props) => <h1>{props.title}</h1>;
+
+//class component
+// class HeadingComponentsUsingClass extends React.Component {
+// 	render() {
+// 		return <h1>Class component - {this.props.title}</h1>;
+// 	}
+// }
+
+// const CurrentTime = () => (
+// 	<p>
+// 		{+new Date()}
+// 		<HeadingComponentsUsingClass title="Inside component" />
+// 	</p>
+// );
+
+// setInterval(() => {
+// 	root.render(
+// 		<React.StrictMode>
+// 			<HeadingComponent title="Hello from component" />
+// 			{/* <HeadingComponentsUsingClass /> */}
+// 			<CurrentTime />
+// 		</React.StrictMode>
+// 	);
+// }, 1000);
+
+const Logo = () => {
+	return (
+		<div id="logo">
+			<img src="google-logo.png" className="logo" alt="Google" />
+		</div>
+	);
+};
+
+// const SearchBar = () => {
+// 	<div id="searchbar">
+// 		<form>
+// 			<input type="text" />
+// 		</form>
+// 	</div>;
+// };
+
+const SearchBar = () => {
+	return (
+		<div id="search-bar">
+			<form className="search-bar">
+				<input type="text" placeholder="Search" />
+			</form>
+		</div>
+	);
+};
+
+const Buttons = () => {
+	return (
+		<div id="buttons">
+			<button className="buttons" type="submit">
+				Search
+			</button>
+			<button className="buttons" type="submit">
+				I am feeling lucky!
+			</button>
+		</div>
+	);
+};
+
+const Languages = (props) => {
+	return (
+		<div id="languages">
+			<h4>Google offered in : </h4>
+			<a href="google.com"> {props.languages}</a>
+		</div>
+	);
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Logo />
+		<SearchBar />
+		<Buttons />
+		<Languages languages="Bengali" />
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
